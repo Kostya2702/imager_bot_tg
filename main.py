dae797ef160eccd19b087b700316844172b861b5
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-07-25 23:17:36 +0300

    Implemented sending a screenshot via the website link

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from email import message
 import logging
-import time
+import re
 import os
+from typing import Text
 
 from aiogram import Bot, Dispatcher, executor, types # URLInputFile
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from urlextract import URLExtract
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+print(ROOT_DIR)
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -21,20 +22,20 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
-def makeScreen(url):
+async def make_screen(url):
         options = webdriver.ChromeOptions()
         options.headless = True
         driver = webdriver.Chrome(options=options)
 
         driver.get(url)
 
-        S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
-        driver.set_window_size(S('Width'),S('Height')) # May need manual adjustment                                                                                                                
-        screenshot = driver.find_element_by_tag_name('body').screenshot('web_screenshot.png')
+        required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+        required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+        driver.set_window_size(required_width, required_height)
+        driver.maximize_window()
+        driver.get_screenshot_as_file('web_screenshot.png')
 
         driver.quit()
-
-        return screenshot
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -43,15 +44,25 @@ async def send_welcome(message: types.Message):
     await message.reply(f"Hi, {message.from_user.first_name}!\nI'm Imager Telegram bot!")
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
+@dp.message_handler(content_types=types.ContentType.ANY)
+async def send_screen(message: types.Message):
 
     extractorURL = URLExtract()
-    url = extractorURL.find_urls(message.text)
-    if url:
-        makeScreen(url)
-        await message.answer('Bee monster is activated!')
-        await message.answer(message.text)
+    url = extractorURL.find_urls(message.text)[0]
+    new_url = re.findall('^http.*', url)
+
+    if len(new_url) == 0:
+        new_url.append(f"https://{url}")
+
+    chat_id = message.chat.id
+
+    print(new_url[0])
+
+    if new_url:
+        await message.reply('Bee monster is activated!\n🪄 Ваш запрос выполняется')
+        await make_screen(new_url[0])
+        with open(f"{ROOT_DIR}/web_screenshot.png", 'rb') as forw_photo:
+            await bot.send_photo(chat_id, forw_photo)
 
 
 if __name__ == '__main__':
